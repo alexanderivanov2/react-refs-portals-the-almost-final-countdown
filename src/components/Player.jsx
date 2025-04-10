ee3cc0b96d40dbe6037c032513516ed85927187a
alexanderivanov2 <alexanderivanov2277@gmail.com>
2025-04-11 00:26:37 +0300

    impleent ref and set player name functionality

--- src/components/Player.jsx
+++ src/components/Player.jsx
@@ -1,10 +1,20 @@
+import { useRef, useState } from 'react'
+
 export default function Player() {
+  const inputName = useRef();
+  const [playerName, setPlayerName] = useState('unknown');
+
+
+  const handleClick = () => {
+    setPlayerName(inputName.current.value);
+  }
+
   return (
     <section id="player">
-      <h2>Welcome unknown entity</h2>
+      <h2>Welcome { playerName ?? 'unknown entity'}</h2>
       <p>
-        <input type="text" />
-        <button>Set Name</button>
+        <input ref={inputName} type="text"/>
+        <button onClick={handleClick}>Set Name</button>
       </p>
     </section>
   );
