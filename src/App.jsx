89ddb00c6a090db36e064949b0ee7a246c4fae0b
alexanderivanov2 <alexanderivanov2277@gmail.com>
2025-04-12 16:50:30 +0300

    implement result modal component

--- src/App.jsx
+++ src/App.jsx
@@ -7,6 +7,9 @@ function App() {
       <Player />
       <div id="challenges">
         <TimerChallenge title="Easy" targetTime={1} />
+        <TimerChallenge title="Not Easy" targetTime={5} />
+        <TimerChallenge title="Getting tough" targetTime={10} />
+        <TimerChallenge title="Pro" targetTime={15} />
       </div>
     </>
   );
---
+++ src/components/ResultModal.jsx
@@ -0,0 +1,24 @@
+import { forwardRef, useImperativeHandle, useRef } from "react"
+
+const ResultModal = forwardRef(function ResultModal({ result, targetTime }, ref) {
+    const dialogRef = useRef();
+    useImperativeHandle(ref, () => {
+        return {
+            open() {
+                dialogRef.current.showModal();
+            }
+        }
+    })
+    return (
+        <dialog ref={dialogRef} className='result-modal'>
+            <h2>You {result}</h2>
+            <p>The target time was <strong>{targetTime}</strong> seconds.</p>
+            <p>You stopped the timer with <strong>X seconds left.</strong></p>
+            <form method="dialog">
+                <button>Close</button>
+            </form>
+        </dialog>
+    )
+})
+
+export default ResultModal
